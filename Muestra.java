823022cfd9f99ae74c49f3e0a93995eb04c9e4e5
ANDREEEEZ <andreziwis2@gmail.com>
2022-01-26 17:44:39 -0500

    bug

--- Muestra.java
+++ Muestra.java
@@ -2,6 +2,7 @@ public class Muestra{
     private String serial;
     private String fecha;
     private int profundidad;
+    private int BUG;
     
     public Muestra(String serial,String fecha,int prof){
      this.serial=serial;
