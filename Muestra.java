3a3348d854fc9d39705e04e5014ff334065adc2f
Werds <santiagoandresxbox@gmail.com>
2022-01-26 17:35:30 -0500

    asd

--- Muestra.java
+++ Muestra.java
@@ -2,9 +2,10 @@ public class Muestra{
     private String serial;
     private String fecha;
     private int profundidad;
+    private int ssss;
     
     public Muestra(String serial,String fecha,int prof){
-     this.serial=serial;
+     this.serial= serial;
      this.fecha=fecha;
      this.profundidad = profundidad;
     }
