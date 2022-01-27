d50d1c7e78089507b8c43c79cb89471e733c17cd
ANDREEEEZ <andreziwis2@gmail.com>
2022-01-26 21:14:05 -0500

    finish

--- Laboratorio.java
+++ Laboratorio.java
@@ -4,20 +4,29 @@ public class Laboratorio {
     private int numeroservi;
 
     public Laboratorio (){
-            Y algunacosa = new Y();
-            this.algo [i]=mes;
-            this.numeroalgos = i;
+        servi = new Servicios [100];
+        for (int i = 0 ; i< 100; i++){
+            Servicios servc = new Servicios();
+            this.servi [i]=servc;
+            this.numeroservi = i;
+        }
     }
-    public void agregar (int v,X p){
-    Y a = algo [v];
-    a.agregar(p);
-    
+
+    public void agregarServicio (int v,Muestra p){
+        Servicios a = servi [v];
+        a.agregarMuestra(p);
     }
+
+    public void registrarMuestra (int nserv, Muestra p){
+        servi [nserv-1].agregarMuestra(p);
+    }
+
     public void mostrarInformes(){
-    algo.imprimir();
-    
+        for (int i = 0; i <= numeroservi; i++){
+            System.out.println("PROMEDIO ESPECIALIZADAS: "+ servi [i].getPromedioEspecializadas());
+            System.out.println("PROMEDIO NO ESPECIAL : "+ servi [i].getPromedioSimples());
+            System.out.println("BLOQUE DE VENtA DEL MES : "+ servi [i]);
+        }
+
     }
-
-
-
 }
