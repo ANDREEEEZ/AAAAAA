bdb56ca5ca3b474e1181397bbaef407e1f2f8d86
ANDREEEEZ <andreziwis2@gmail.com>
2022-01-26 20:36:44 -0500

    muestra

---
+++ Muestra.java
@@ -0,0 +1,35 @@
+public class Muestra {
+
+
+    private String serial;
+    private String fecha;
+    private int profundidad;
+    
+    public void setSerial (String serial){
+        this.serial=serial  ;
+    }
+    public void setFecha (String fecha){
+        this.fecha = fecha ;
+    }
+    public void setProf (int profundidad){
+        this.profundidad = profundidad;
+    }
+    public String getSerial (){
+    
+        return serial;
+    }
+    public String getFecha (){
+    
+        return fecha;
+    }
+    public int getProf(){
+    
+        return profundidad;
+    }
+
+
+
+
+
+
+}
